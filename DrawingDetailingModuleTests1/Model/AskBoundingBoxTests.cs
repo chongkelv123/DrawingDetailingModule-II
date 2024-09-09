d4d86bbb858222938e7f6fff240a1f1cf9ba3b04
chongkelv <e0895806@u.nus.edu>
2024-09-09 08:40:38 +0800

    Add Unit test for AskBoudingBoxTests and MachiningDescriptionModelTests.

--- DrawingDetailingModuleTests1/Model/AskBoundingBoxTests.cs
+++ DrawingDetailingModuleTests1/Model/AskBoundingBoxTests.cs
@@ -66,5 +66,68 @@ namespace DrawingDetailingModule.Model.Tests
             // Assert
             Assert.AreEqual(true, result);
         }
+
+        [TestMethod()]
+        public void AskDirectionTest_ZvalueEqualMinZ_ReturnTrue()
+        {
+            // Arrange
+            AskBoundingBox askBounding = new AskBoundingBox();
+            askBounding.MinX = 0;
+            askBounding.MinY = -177.5;
+            askBounding.MinZ = -35.0;
+            askBounding.MaxX = 415;
+            askBounding.MaxY = 177.5;
+            askBounding.MaxZ = 0;
+
+            // Act
+            double[] point = new double[3] { 0, 0, -35 };
+            double[] result = askBounding.AskDirection(point, AXIS.Z);
+            double[] expected = new double[3] { 0, 0, 1 };
+
+            // Assert
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void AskDirectionTest_ZvalueEqualMaxZ_ReturnTrue()
+        {
+            // Arrange
+            AskBoundingBox askBounding = new AskBoundingBox();
+            askBounding.MinX = 0;
+            askBounding.MinY = -177.5;
+            askBounding.MinZ = -35.0;
+            askBounding.MaxX = 415;
+            askBounding.MaxY = 177.5;
+            askBounding.MaxZ = 0;
+
+            // Act
+            double[] point = new double[3] { 0, 0, 0 };
+            double[] result = askBounding.AskDirection(point, AXIS.Z);
+            double[] expected = new double[3] { 0, 0, -1 };
+
+            // Assert
+            CollectionAssert.AreEqual(expected, result);
+        }
+        
+        [TestMethod()]
+        public void AskDirectionTest_ZvalueInTheMiddle_ReturnTrue()
+        {
+            // Arrange
+            AskBoundingBox askBounding = new AskBoundingBox();
+            askBounding.MinX = 0;
+            askBounding.MinY = -177.5;
+            askBounding.MinZ = -35.0;
+            askBounding.MaxX = 415;
+            askBounding.MaxY = 177.5;
+            askBounding.MaxZ = 0;
+
+            // Act
+            double[] point = new double[3] { 0, 0, 10 };
+            double[] result = askBounding.AskDirection(point, AXIS.Z);
+            double[] expected = new double[3] { 0, 0, -1 };
+
+            // Assert
+            CollectionAssert.AreEqual(expected, result);
+        }
     }
 }
---
+++ DrawingDetailingModuleTests1/Model/MachiningDescriptionModelTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DrawingDetailingModule.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawingDetailingModule.Model.Tests
+{
+    [TestClass()]
+    public class MachiningDescriptionModelTests
+    {
+        [TestMethod()]
+        public void GetWCStartPointDiameterTest()
+        {
+            string input = "WC <o>6.00 H7 S/C, (<O>3 wc sp)";
+            MachiningDescriptionModel descriptionModel = new MachiningDescriptionModel();
+
+            double result = descriptionModel.GetWCStartPointDiameter(input);
+
+            Assert.AreEqual(3.0, result);
+        }
+    }
+}
